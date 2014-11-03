45a919c12176f64bcc78cf528abf4a1a9ff009c9
Chen Huang <alan.tolearn@gmail.com>
2014-11-03 16:50:49 -0500

    User can create new regions
    * add gem "pry"

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -2,4 +2,23 @@ class RegionsController < ApplicationController
   def index
     @regions = Region.all
   end
+
+  def new
+    @region = Region.new
+  end
+
+  def create
+    region = Region.new(region_params)
+    if region.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def region_params
+    params.require(:region).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 
-  resources :regions, only: [:index]
+  resources :regions, only: [:index, :new, :create]
 
   root "regions#index"
 end
