5ac4fd714f9677fa04d2daad86a1a389e3e0b6cc
Dan Frenette <paradox815@gmail.com>
2014-11-04 17:01:25 -0500

    User can update and destroy categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,32 @@
+class CategoriesController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+  skip_before_action :require_admin, only: [:new, :create]
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      redirect_to region_path(params[:region_id])
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    category = Category.find(params[:id])
+    category.destroy
+
+    redirect_to region_path(params[:region_id])
+
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
