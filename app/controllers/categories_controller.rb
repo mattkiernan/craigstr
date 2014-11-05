242f88250a6c129793e32918321b91bccb5b0afa
mattkiernan <matt.kiernan@gmail.com>
2014-11-05 15:59:43 -0500

    Add instace variables to category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,10 +7,10 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    region = load_region_from_url
-    category = region.categories.new(category_params)
-    if category.save
-      redirect_to region
+    @region = load_region_from_url
+    @category = @region.categories.new(category_params)
+    if @category.save
+      redirect_to @region
     else
       render :new
     end
@@ -28,11 +28,11 @@ class CategoriesController < ApplicationController
   end
 
   def update
-    region = load_region_from_url
-    category = load_category_from_url
+    @region = load_region_from_url
+    @category = load_category_from_url
 
-    if category.update(category_params)
-      redirect_to region
+    if @category.update(category_params)
+      redirect_to @region
     else
       render :edit
     end
